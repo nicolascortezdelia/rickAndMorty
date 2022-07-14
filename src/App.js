7ecc87ab95e2a07d383a362ec91ef82f4bfdf253
Nicolas Cortez D Elia <nicocdelia@gmail.com>
2022-07-14 20:32:03 -0300

    agrego carpeta providers routes pages, home, instalo router DOM

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
-     <h1>React Advanced.</h1>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+        </Routes>
+      </BrowserRouter>
   );
 };
 
